f12a6d55a88f11accc7323aff42d643ff98ac2f2
emsj06001 <emsj06001@E-PW024NWP>
2023-09-15 12:31:59 +0200

    Add project files.

---
+++ uppgift2.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace uppgift_2_2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin i längdhopp, svara i meter?...");
+            String x = Console.ReadLine();
+            Console.WriteLine("Hur långt hoppade Alma i längdhopp, svara i meter?...");   
+            String y = Console.ReadLine();
+
+            double hopp1 = double.Parse(x);
+            double hopp2 = double.Parse(y);
+            double sum = hopp1 - hopp2;
+            Console.WriteLine("Elin hoppade "+ sum +" meter längre än Alma.");
+            Console.ReadKey();
+        }
+    }
+}
